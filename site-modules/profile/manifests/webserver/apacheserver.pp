2e38714f0cbbc51c71a9f295a7d4ca25425f7d00
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2022-12-09 00:08:01 -0500

    Update and rename example.pp to apacheserver.pp

--- site-modules/profile/manifests/webserver/apacheserver.pp
+++ site-modules/profile/manifests/webserver/apacheserver.pp
@@ -1,5 +1,5 @@
-# /etc/puppetlabs/code/environments/production/site/profile/manifests/webserver/example.pp
-class profile::apacheserver::example (
+# /etc/puppetlabs/code/environments/production/site/profile/manifests/webserver/apacheserver.pp
+class profile::apacheserver (
   String $content      = "Hello from vhost\\n",
   Array[String] $ports = ['80']
   Array[String] $ips   = ['127.0.0.1','127.0.0.2'],
