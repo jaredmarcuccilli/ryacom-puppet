79d4acd53d4579c53b0ebd8342872d271b3906c6
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2022-12-09 00:24:44 -0500

    Update apacheserver.pp

--- site-modules/profile/manifests/apacheserver.pp
+++ site-modules/profile/manifests/apacheserver.pp
@@ -1,22 +1,9 @@
-# /etc/puppetlabs/code/environments/production/site/profile/manifests/webserver/apacheserver.pp
-class profile::apacheserver (
-  String $content      = "Hello from vhost\\n",
-  Array[String] $ports = ['80'],
-  Array[String] $ips   = ['127.0.0.1','127.0.0.2']
-) 
-{
+class profile::apacheserver {
   class { 'apache':
     default_vhost => false,
   }
-{
-  apache::vhost { 'example.com':
-    port     => $ports,
-    ip       => $ips,
-    ip_based => true,
-    docroot  => '/var/www/html',
-  }
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => $content,
+  apache::vhost { 'mynode.company.com':
+    port    => '80',
+    docroot => '/var/www/mynode.company.com',
   }
 }
