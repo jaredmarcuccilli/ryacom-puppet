7368e62f9b39a74270ecc0fc2e7635ab030ef6c8
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2022-12-14 21:37:56 -0500

    Update and rename zabbix6-client-role.pp to zabbix6_client.pp

--- site-modules/role/manifests/zabbix6-client-role.pp
+++
@@ -1,4 +0,0 @@
-class role::zabbix6clientrole {
-  include profile::base
-  include profile::zabbix6clientprofile
-}
---
+++ site-modules/role/manifests/zabbix6_client.pp
@@ -0,0 +1,4 @@
+class role::zabbix6_client {
+  include profile::base
+  include profile::zabbix6_client
+}
