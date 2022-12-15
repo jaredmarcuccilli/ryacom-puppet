304e1d95970ca3f0bc42d67c42870a4a964557cd
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2022-12-14 21:38:40 -0500

    Update and rename zabbix6-client-profile.pp to zabbix6_client.pp

--- site-modules/profile/manifests/zabbix6_client.pp
+++ site-modules/profile/manifests/zabbix6_client.pp
@@ -1,7 +1,7 @@
-class profile::zabbix6clientprofile {
+class profile::zabbix6_client {
   class { 'zabbix::agent':
     server => 'zabbix.ad.ryacom.org',
-    zabbix_version       => '6.0',
+    manage_firewall      => true,
     manage_repo          => true,
     zabbix_package_state => 'latest',
   }
