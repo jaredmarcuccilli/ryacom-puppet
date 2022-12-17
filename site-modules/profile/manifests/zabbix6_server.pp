839ba5b0cdd2731cadaac382728d261eff271a91
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2022-12-16 19:46:28 -0500

    Update zabbix6_server.pp

--- site-modules/profile/manifests/zabbix6_server.pp
+++ site-modules/profile/manifests/zabbix6_server.pp
@@ -1,8 +1,8 @@
 class profile::zabbix6_server {
   file_line { 'line_replace':
     path  => '/usr/share/zabbix/vendor/onelogin/php-saml/src/Saml2/Settings.php',
-    match => "            \$this->_security['requestedAuthnContext'] = true;",
-    line  => "            \$this->_security['requestedAuthnContext'] = false;",
+    match => "            \\$this->_security['requestedAuthnContext'] = true;",
+    line  => "            \\$this->_security['requestedAuthnContext'] = false;",
     append_on_no_match => false,
   }
 }
