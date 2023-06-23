8860a611d0c5df96b61293505884f855fc91258d
Jared Marcuccilli <jaredmarcuccilli@users.noreply.github.com>
2023-06-22 22:55:32 -0400

    Delete apacheserver.pp

--- site-modules/profile/manifests/apacheserver.pp
+++
@@ -1,9 +0,0 @@
-class profile::apacheserver {
-  class { 'apache':
-    default_vhost => false,
-  }
-  apache::vhost { 'mynode.company.com':
-    port    => '80',
-    docroot => '/var/www/mynode.company.com',
-  }
-}
